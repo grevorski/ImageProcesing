7057df039f7b4d0e28ac96c965c9ecf899d57e75
grevorski <74430404+grevorski@users.noreply.github.com>
2021-06-10 10:49:59 +0200

    update

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -8,24 +8,32 @@ public class Menu {
         System.out.println("Write path to image you want to process: ");
         String path = sc.nextLine();
         Processing processing = new Processing(path);
+        boolean exit = true;
+
+        while(exit){
         System.out.println("Processing Methods:\n 1. Regionprops (only monochrome)\n 2. Kirsch filtration (RGB or mono)\n" +
-                "3. Zamkniecie elementem linijnym\n 4.Mapa odległości geodezyjnej");
+                "3. Zamkniecie elementem linijnym\n 4.Mapa odległości geodezyjnej\n5.exit");
         int choice = sc.nextInt();
-        switch(choice){
-            case 1:
-                processing.regionProps();
-                break;
-            case 2:
-                processing.kirschFiltration();
-                break;
-            case 3:
-                processing.ClosingLinear();
-                break;
-            case 4:
-                processing.GeodeticDistanceMap();
-                break;
-            default:
-                System.out.println("Wrong input");
+            switch(choice){
+                case 1:
+                    processing.regionProps();
+                    break;
+                case 2:
+                    processing.kirschFiltration();
+                    break;
+                case 3:
+                    processing.ClosingLinear();
+                    break;
+                case 4:
+                    processing.GeodeticDistanceMap();
+                    break;
+                case 5:
+                    exit = false;
+                    break;
+                default:
+                    System.out.println("Wrong input");
+            }
         }
+
     }
 }
